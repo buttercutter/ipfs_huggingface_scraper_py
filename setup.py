3ccc1d4a2c8490507cffb34e42114a79a97b8d85
endomorphosis <starworks5@gmail.com>
2025-04-04 00:34:23 -0700

    chore: Add subproject metadata and clean up dependencies in setup.py

--- setup.py
+++ setup.py
@@ -4,17 +4,11 @@ setup(
 	name='ipfs_huggingface_scraper_py',
 	version='0.0.3',
 	packages=[
-		'ipfs_huggingface_scraper_py    ',
+		'ipfs_huggingface_scraper_py',
 	],
 	install_requires=[
-        'ipfs_datasets_py',
-        'ipfs_transfrormers_py',
-        "ipfs_model_manager_py",
         "orbitdb_kit_py",
 		"ipfs_kit_py",
-		"faiss",
-        "ipfs_faiss_py",
-		'datasets',
 		'urllib3',
 		'requests',
 		'boto3',
